1870b419fd069e9c3003703a7c3b7dd1003ba5af
Ashish Karki <4175758+ashishkarki@users.noreply.github.com>
2020-10-13 18:23:53 +0545

    Updated TransferEntityBuilderTest's tests with proper displayname

--- src/test/java/com/karkia/balancer/utils/TransferEntityBuilderTest.java
+++ src/test/java/com/karkia/balancer/utils/TransferEntityBuilderTest.java
@@ -2,7 +2,10 @@ package com.karkia.balancer.utils;
 
 import com.karkia.balancer.entities.TransferEntity;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.time.LocalDate;
 
@@ -10,6 +13,7 @@ import static com.karkia.balancer.Constants.TRANSFER_DATE_FORMAT;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+@ExtendWith(SpringExtension.class)
 class TransferEntityBuilderTest {
     final String NON_STARTING_TRANSFER_STR = "112233, 223344, 11.11, 11/08/2055, 1448";
     final TransferEntity SAMPLE_TRANSFER_ENTITY = TransferEntity.builder()
@@ -21,19 +25,22 @@ class TransferEntityBuilderTest {
             .build();
 
     @Test
-    void buildTransferEntity_is_setup_correctly() {
+    @DisplayName("buildTransferEntity() is setup correctly and returns expected value")
+    void buildTransferEntity_test1() {
         assertNotNull(TransferEntityBuilder.buildTransferEntity(NON_STARTING_TRANSFER_STR));
     }
 
     @Test
-    void buildTransferEntity_should_fail_on_empty_transferString() {
+    @DisplayName("buildTransferEntity() should fail on empty argument string")
+    void buildTransferEntity_test2() {
         Assertions.assertThrows(AssertionError.class, () -> {
             TransferEntityBuilder.buildTransferEntity("");
         });
     }
 
     @Test
-    void buildTransferEntity_should_work_with_proper_transferString() {
+    @DisplayName("buildTransferEntity() should work with valid argument string")
+    void buildTransferEntity_test3() {
         final var actualEntity = TransferEntityBuilder.buildTransferEntity(NON_STARTING_TRANSFER_STR);
         assertEquals(SAMPLE_TRANSFER_ENTITY, actualEntity);
     }
